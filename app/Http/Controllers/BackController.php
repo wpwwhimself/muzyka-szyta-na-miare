e58d11fbb893891b97d1401786030b351a87f1b5
Wojciech Przybyła <contact@wpww.pl>
2022-10-18 01:00:24 +0200

    Current quests

--- app/Http/Controllers/BackController.php
+++ app/Http/Controllers/BackController.php
@@ -10,43 +10,27 @@ use Illuminate\Support\Facades\Auth;
 class BackController extends Controller
 {
     public function dashboard(){
-        if(Auth::id() == 1){
-            $quests = Quest::join("songs", "quests.song_id", "=", "songs.id")
-                ->join("clients", "quests.client_id", "=", "clients.id")
-                ->join("statuses", "quests.status_id", "=", "statuses.id")
-                ->orderByRaw("case when deadline is null then 1 else 0 end, deadline")
-                ->get([
-                    "quests.id",
-                    "title",
-                    "artist",
-                    "client_name",
-                    "surname",
-                    "status_id",
-                    "status_name",
-                    "price",
-                    "paid",
-                    "deadline"
-                ]);
-        }else{
+        $quests = Quest::join("songs", "quests.song_id", "=", "songs.id")
+            ->join("clients", "quests.client_id", "=", "clients.id")
+            ->join("statuses", "quests.status_id", "=", "statuses.id")
+            ->whereRaw("status_id not in (7, 8, 18, 9, 19)")
+            ->orderByRaw("case when deadline is null then 1 else 0 end, deadline");
+        if(Auth::id() != 1){
             $client_id = Client::where("user_id", Auth::id())->value("id");
-            $quests = Quest::join("songs", "quests.song_id", "=", "songs.id")
-                ->join("clients", "quests.client_id", "=", "clients.id")
-                ->join("statuses", "quests.status_id", "=", "statuses.id")
-                ->where("client_id", $client_id)
-                ->orderByRaw("case when deadline is null then 1 else 0 end, deadline")
-                ->get([
-                    "quests.id",
-                    "title",
-                    "artist",
-                    "client_name",
-                    "surname",
-                    "status_id",
-                    "status_name",
-                    "price",
-                    "paid",
-                    "deadline"
-                ]);
+            $quests = $quests->where("client_id", $client_id);
         }
+        $quests = $quests->get([
+            "quests.id",
+            "title",
+            "artist",
+            "client_name",
+            "surname",
+            "status_id",
+            "status_name",
+            "price",
+            "paid",
+            "deadline"
+        ]);
 
         return view("dashboard", [
             "title" => "Podsumowanie",

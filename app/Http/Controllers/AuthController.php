8ecbc23c67548682acc127335953014bf1127115
Wojciech Przybyła <contact@wpww.pl>
2022-10-22 23:53:26 +0200

    Eye candy with session

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -53,6 +53,6 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect("/");
+        return redirect("/")->with("success", "Wylogowano");
     }
 }

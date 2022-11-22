f502dbcb416ed6b6f349d5696cb788acd03defd5
Wojciech Przybyła T <contact@wpww.pl>
2022-11-22 18:57:33 +0100

    indentation correction

--- resources/views/client/quests.blade.php
+++ resources/views/client/quests.blade.php
@@ -1,57 +1,60 @@
 @extends('layouts.app', compact("title"))
 
 @section('content')
-    @foreach (["success", "error"] as $status)
-    @if (session($status))
-        <x-alert :status="$status" />
-    @endif
-    @endforeach
-        <section id="quests-list">
-            <div class="section-header">
-                <h1><i class="fa-solid fa-boxes-stacked"></i> Lista zleceń</h1>
-                <div>
-                    <x-a href="{{ route('add-request') }}" icon="plus">Dodaj nowe zapytanie</x-a>
-                </div>
-            </div>
-            <style>
-            .table-row{ grid-template-columns: 3em 3fr 11em 2em; }
-            .table-row span:nth-child(5){ text-align: center; }
-            </style>
-            <div class="quests-table">
-                <div class="table-header table-row">
-                    <span>Typ</span>
-                    <span>Tytuł<br>Wykonawca</span>
-                    <span><i class="fa-solid fa-traffic-light"></i> Status</span>
-                    <span @popper(Czy opłacony)><i class="fa-solid fa-sack-dollar"></i></span>
-                </div>
-                <hr />
-                @forelse ($quests as $quest)
-                <a href="{{ route('quest', $quest->id) }}" class="table-row p-{{ $quest->status_id }}">
-                    <span>
-                        <x-quest-type
-                            :id="song_quest_type($quest->song_id)->id ?? 0"
-                            :label="song_quest_type($quest->song_id)->type ?? 'nie zdefiniowano'"
-                            :fa-symbol="song_quest_type($quest->song_id)->fa_symbol ?? 'fa-circle-question'"
-                            />
-                    </span>
-                    <span>
-                        <h3 class="song-title">{{ $quest->song->title ?? "bez tytułu" }}</h3>
-                        <span class="song-artist">{{ $quest->song->artist }}</span>
-                    </span>
-                    <span class="quest-status">
-                        <x-phase-indicator :status-id="$quest->status_id" :small="true" />
-                    </span>
-                    <span>
-                    @if (quest_paid($quest->id, $quest->price))
-                    <i class="quest-paid fa-solid fa-check"></i>
-                    @endif
-                    </span>
-                </a>
-                @empty
-                <p class="grayed-out">brak zapytań</p>
-                @endforelse
 
-            </div>
-            {{ $quests->links() }}
-        </section>
+@foreach (["success", "error"] as $status)
+@if (session($status))
+    <x-alert :status="$status" />
+@endif
+@endforeach
+
+<section id="quests-list">
+    <div class="section-header">
+        <h1><i class="fa-solid fa-boxes-stacked"></i> Lista zleceń</h1>
+        <div>
+            <x-a href="{{ route('add-request') }}" icon="plus">Dodaj nowe zapytanie</x-a>
+        </div>
+    </div>
+    <style>
+    .table-row{ grid-template-columns: 3em 3fr 11em 2em; }
+    .table-row span:nth-child(5){ text-align: center; }
+    </style>
+    <div class="quests-table">
+        <div class="table-header table-row">
+            <span>Typ</span>
+            <span>Tytuł<br>Wykonawca</span>
+            <span><i class="fa-solid fa-traffic-light"></i> Status</span>
+            <span @popper(Czy opłacony)><i class="fa-solid fa-sack-dollar"></i></span>
+        </div>
+        <hr />
+        @forelse ($quests as $quest)
+        <a href="{{ route('quest', $quest->id) }}" class="table-row p-{{ $quest->status_id }}">
+            <span>
+                <x-quest-type
+                    :id="song_quest_type($quest->song_id)->id ?? 0"
+                    :label="song_quest_type($quest->song_id)->type ?? 'nie zdefiniowano'"
+                    :fa-symbol="song_quest_type($quest->song_id)->fa_symbol ?? 'fa-circle-question'"
+                    />
+            </span>
+            <span>
+                <h3 class="song-title">{{ $quest->song->title ?? "bez tytułu" }}</h3>
+                <span class="song-artist">{{ $quest->song->artist }}</span>
+            </span>
+            <span class="quest-status">
+                <x-phase-indicator :status-id="$quest->status_id" :small="true" />
+            </span>
+            <span>
+            @if (quest_paid($quest->id, $quest->price))
+            <i class="quest-paid fa-solid fa-check"></i>
+            @endif
+            </span>
+        </a>
+        @empty
+        <p class="grayed-out">brak zapytań</p>
+        @endforelse
+
+    </div>
+    {{ $quests->links() }}
+</section>
+
 @endsection
--- resources/views/client/requests.blade.php
+++ resources/views/client/requests.blade.php
@@ -1,46 +1,48 @@
 @extends('layouts.app', compact("title"))
 
 @section('content')
-    @foreach (["success", "error"] as $status)
-    @if (session($status))
-        <x-alert :status="$status" />
-    @endif
-    @endforeach
-        <section id="requests-list">
-            <div class="section-header">
-                <h1><i class="fa-solid fa-envelope-open-text"></i> Lista zapytań</h1>
-                <div>
-                    <x-a href="{{ route('add-request') }}" icon="plus">Dodaj nowe</x-a>
-                </div>
-            </div>
-            <style>
-            .table-row{ grid-template-columns: 3em 4fr 11em; }
-            </style>
-            <div class="quests-table">
-                <div class="table-header table-row">
-                    <span>Typ</span>
-                    <span>Tytuł<br>Wykonawca</span>
-                    <span><i class="fa-solid fa-traffic-light"></i> Status</span>
-                </div>
-                <hr />
-                @forelse ($requests as $request)
-                <a href="{{ route("request", $request->id) }}" class="table-row p-{{ $request->status_id }}">
-                    <span>
-                        <x-quest-type :id="$request->quest_type_id" :label="$request->quest_type->type" />
-                    </span>
-                    <span>
-                        <h3 class="song-title">{{ $request->title }}</h3>
-                        <span class="song-artist">{{ $request->cover_artist ?? $request->artist }}</span>
-                    </span>
-                    <span class="quest-status">
-                        <x-phase-indicator :status-id="$request->status_id" :small="true" />
-                    </span>
-                </a>
-                @empty
-                <p class="grayed-out">brak zapytań</p>
-                @endforelse
-                </tbody>
-            </div>
-        </section>
+
+@foreach (["success", "error"] as $status)
+@if (session($status))
+    <x-alert :status="$status" />
+@endif
+@endforeach
+
+<section id="requests-list">
+    <div class="section-header">
+        <h1><i class="fa-solid fa-envelope-open-text"></i> Lista zapytań</h1>
+        <div>
+            <x-a href="{{ route('add-request') }}" icon="plus">Dodaj nowe</x-a>
+        </div>
     </div>
+    <style>
+    .table-row{ grid-template-columns: 3em 4fr 11em; }
+    </style>
+    <div class="quests-table">
+        <div class="table-header table-row">
+            <span>Typ</span>
+            <span>Tytuł<br>Wykonawca</span>
+            <span><i class="fa-solid fa-traffic-light"></i> Status</span>
+        </div>
+        <hr />
+        @forelse ($requests as $request)
+        <a href="{{ route("request", $request->id) }}" class="table-row p-{{ $request->status_id }}">
+            <span>
+                <x-quest-type :id="$request->quest_type_id" :label="$request->quest_type->type" />
+            </span>
+            <span>
+                <h3 class="song-title">{{ $request->title }}</h3>
+                <span class="song-artist">{{ $request->cover_artist ?? $request->artist }}</span>
+            </span>
+            <span class="quest-status">
+                <x-phase-indicator :status-id="$request->status_id" :small="true" />
+            </span>
+        </a>
+        @empty
+        <p class="grayed-out">brak zapytań</p>
+        @endforelse
+        </tbody>
+    </div>
+</section>
+
 @endsection

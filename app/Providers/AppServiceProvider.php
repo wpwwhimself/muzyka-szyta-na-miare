00508874bfc84bec9534967d077435072d8a725f
Wojciech Przybyła T <contact@wpww.pl>
2022-12-06 21:58:35 +0100

    Pagination broke, trying to fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::defaultView("vendor\pagination\default");
+        Paginator::defaultView("vendor.pagination.default");
         Paginator::defaultSimpleView("vendor\pagination\default");
     }
 }

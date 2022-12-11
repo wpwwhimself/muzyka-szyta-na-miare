da8cb6028288608a576009ad7d7b723c54f10675
Wojciech Przybyła T <contact@wpww.pl>
2022-12-11 22:17:05 +0100

    Showcases list

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,10 +12,10 @@ use Illuminate\Support\Facades\DB;
 class HomeController extends Controller
 {
     public function index(){
-        $showcases = Showcase::orderBy("quest_id", "desc")->orderBy("updated_at", "desc")->limit(3)->get();
+        $showcases = Showcase::orderBy("song_id", "desc")->orderBy("updated_at", "desc")->limit(3)->get();
 
         $prices = DB::table("prices")->where("operation", "+")->get(["service", "quest_type_id", "price_".strtolower(CURRENT_PRICING())." AS price"]);
-        
+
         $quest_types_raw = QuestType::all()->toArray();
         foreach($quest_types_raw as $val){
             $quest_types[$val["id"]] = $val["type"];

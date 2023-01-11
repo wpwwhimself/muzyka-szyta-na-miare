4d20c60b5110ae3f1e9dc3f8f7e35ab812f915cb
Wojciech Przybyla <wojciech.przybyla@questy.pl>
2023-01-11 15:34:16 +0100

    Client sees hints for add request fields

--- resources/views/client/add-request.blade.php
+++ resources/views/client/add-request.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.app', compact("title"))
 
 @section('content')
+    @php
+        $hint['link'] = ["Link do nagrania" => "Podaj link do oryginalnego wykonania, na którym mam opierać pracę, np. Youtube"];
+        $hint['wishes'] = ["Życzenia do zlecenia" => "Wszelkie uwagi związane z projektem: zmiana transpozycji, dodanie linii melodycznej, inna instrumentalizacja itp."];
+        $hint['deadline'] = ["Termin wykonania" => "Jeśli Twoje zlecenie jest pilne, tu możesz podać najpóźniejszy termin odebrania plików. Jeśli nie masz takich ograniczeń, pozostaw puste"];
+    @endphp
     <form method="post" action={{ route("mod-request-back") }}>
         @csrf
         <h1>Dodaj nowe zapytanie</h1>
@@ -10,9 +15,9 @@
                 <x-select name="quest_type" label="Rodzaj zlecenia" :options="$questTypes" :required="true" :small="true" />
                 <x-input type="text" name="title" label="Tytuł utworu" />
                 <x-input type="text" name="artist" label="Oryginalny wykonawca" />
-                <x-input type="text" name="link" label="Link do nagrania" :small="true" />
-                <x-input type="TEXT" name="wishes" label="Życzenia" value="{{ Auth::user()->client->default_wishes }}" />
-                <x-input type="date" name="hard_deadline" label="Opcjonalny termin wykonania" />
+                <x-input type="text" name="link" label="Link do nagrania" :small="true" :hint="$hint['link']" />
+                <x-input type="TEXT" name="wishes" label="Życzenia" value="{{ Auth::user()->client->default_wishes }}" :hint="$hint['wishes']" />
+                <x-input type="date" name="hard_deadline" label="Opcjonalny termin wykonania" :hint="$hint['deadline']" />
             </section>
         </div>
         <input type="hidden" name="modifying" value="0" />

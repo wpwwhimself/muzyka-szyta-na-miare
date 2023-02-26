24b1bbe999e09ae23aa460c6a14c46f924a3fa66
Wojciech Przybyła T <contact@wpww.pl>
2023-02-26 20:23:51 +0100

    Settings' descriptions

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -17,38 +17,47 @@ class SettingsSeeder extends Seeder
         DB::table("settings")->insert([
             [
                 "setting_name" => "veteran_from",
+                "desc" => "Po ilu ukończonych zleceniach klient staje się stałym klientem",
                 "value_str" => 10
             ],
             [
                 "setting_name" => "current_pricing",
+                "desc" => "Nazwa obecnego cennika, do którego przypisywani są nowi klienci",
                 "value_str" => "B"
             ],
             [
                 "setting_name" => "pricing_B_since",
+                "desc" => "Od kiedy obowiązuje cennik B",
                 "value_str" => "2022-09-09"
             ],
             [
                 "setting_name" => "request_expired_after",
+                "desc" => "Po ilu dniach Sprzątacz wygasza porzucone zapytania",
                 "value_str" => 5
             ],
             [
                 "setting_name" => "quest_expired_after",
+                "desc" => "Po ilu dniach Sprzątacz wygasza porzucone zlecenia",
                 "value_str" => 30
             ],
             [
                 "setting_name" => "quest_reminder_time",
+                "desc" => "Po ilu dniach Sprzątacz ponawia prośbę o ocenę dla zleceń",
                 "value_str" => 5
             ],
             [
                 "setting_name" => "workdays_free",
+                "desc" => "Które dni tygodnia są traktowane jako wolne i nie są brane pod uwagę przy liczeniu deadline'u",
                 "value_str" => "2,5"
             ],
             [
                 "setting_name" => "available_days_needed",
+                "desc" => "Ile (dostępnych) dni od dziś można proponować deadline",
                 "value_str" => 2
             ],
             [
                 "setting_name" => "available_day_until",
+                "desc" => "Ile maksymalnie zleceń dziennie przyjmuję (dni z tyloma+ zleceniami nie są brane pod uwagę przy liczeniu deadline'u)",
                 "value_str" => 3
             ],
         ]);

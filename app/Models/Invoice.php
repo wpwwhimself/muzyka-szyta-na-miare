8271e26a30dc5d8352b2057ea8a3e79799117219
Wojciech Przybyła <contact@wpww.pl>
2023-05-10 12:03:12 +0200

    invoices - code adapted to changing db structure

--- app/Models/Invoice.php
+++ app/Models/Invoice.php
@@ -11,14 +11,14 @@ class Invoice extends Model
 
     protected $fillable = [
         "full_code_override",
-        "primary", "visible",
+        "visible",
         "amount", "paid",
         "payer_name", "payer_title", "payer_address", "payer_nip", "payer_regon",
         "payer_email", "payer_phone",
     ];
 
     public function getFullCodeAttribute(){
-        return $this->full_code_override ?? $this->id . "/" . ($this->primary ? "F" : "FD") . "/" . $this->quest_id;
+        return $this->full_code_override ?? $this->id . "/F/" . $this->created_at->format("y");
     }
     public function getIsPaidAttribute(){
         return $this->amount == $this->paid;

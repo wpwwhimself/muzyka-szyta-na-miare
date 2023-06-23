94e7f5ff7df95340a4f7935bd157b160d2564c3e
Wojciech Przybyła <contact@wpww.pl>
2023-06-23 17:58:27 +0200

    ppp - mobile compatibility

--- resources/views/archmage/ppp.blade.php
+++ resources/views/archmage/ppp.blade.php
@@ -2,10 +2,7 @@
 
 @section('content')
 
-<div id="ppp" class="grid-2">
-  <div id="ppp_content">
-    {!! Illuminate\Mail\Markdown::parse($content) !!}
-  </div>
+<div id="ppp" class="flex-right">
   <div id="ppp_nav" class="section-like">
     <h2>Rozdziały</h2>
     <ol>
@@ -13,6 +10,9 @@
       <li value="{{ explode("_", $ttl)[0] }}"><a href="{{ route('ppp', ['c' => $ttl]) }}">{{ explode("_", $ttl)[1] }}</a></li>
     @endforeach
     </ol>
+  </div>
+  <div id="ppp_content">
+    {!! Illuminate\Mail\Markdown::parse($content) !!}
   </div>
 </div>
 

ba9440ae103ff95d57adb706c306e4bba0f261af
Wojciech Przybyła <contact@wpww.pl>
2023-08-24 17:39:50 +0200

    bug bounty

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,6 +4,7 @@
     <h2>{{ config("app.name") }}</h2>
     <p>Stronę własnoręcznie zbudował <a href="http://wpww.pl">Wojciech Przybyła</a></p>
     <p><a href="https://creativecommons.org/licenses/by-sa/3.0/pl/">&copy; CC BY-SA 3.0</a> 2019 – {{ date("Y") }}</p>
+    <p class="yellowed-out"><i class="fas fa-bug"></i> Widzisz gdzieś błąd? Napisz!</p>
   </div>
   <div class="contact-info">
     <a href="mailto:kontakt@muzykaszytanamiare.pl">

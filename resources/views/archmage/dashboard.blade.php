1116a2cdb70c5c566043a856ae959d9e15c43eb0
Wojciech Przybyła <contact@wpww.pl>
2023-10-17 12:05:57 +0200

    dashboard - things worth attention even higher

--- resources/views/archmage/dashboard.blade.php
+++ resources/views/archmage/dashboard.blade.php
@@ -9,6 +9,37 @@
             />
         </div>
 
+        @if (count($patrons_adepts) > 0)
+        <section id="patrons-adepts" style="grid-column: 1 / span 2">
+            <div class="section-header">
+                <h1><i class="fa-solid fa-chalkboard-user"></i> Potencjalni patroni</h1>
+                <div>
+                    <x-a href="https://www.facebook.com/muzykaszytanamiarepl/reviews" target="_blank">Recenzje</x-a>
+                </div>
+            </div>
+            <table>
+                <thead>
+                    <th>Klient</th>
+                    <th>Decyzja</th>
+                </thead>
+                <tbody>
+                    @foreach ($patrons_adepts as $patron)
+                    <tr>
+                        <td>
+                            <i class="fa-solid fa-{{ $patron->is_veteran ? 'user-shield' : 'user' }}"></i>
+                            <a href="{{ route('clients', ['search' => $patron->id]) }}">{{ _ct_($patron->client_name) }}</a>
+                        </td>
+                        <td>
+                            <x-button label="" icon="check" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 2]) }}" :small="true" />
+                            <x-button label="" icon="x" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 0]) }}" :small="true" />
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </section>
+        @endif
+
         <section id="recent">
             <div class="section-header">
                 <h1><i class="fa-solid fa-clock-rotate-left"></i> Ostatnie zmiany</h1>
@@ -57,9 +88,7 @@
                     @endforelse
                 </tbody>
             </table>
-        </section>
 
-        <section id="dashboard-janitor-log">
             <div class="section-header">
                 <h1><i class="fa-solid fa-broom"></i> Raport Sprzątacza</h1>
             </div>
@@ -121,36 +150,108 @@
             </table>
         </section>
 
-        @if (count($patrons_adepts) > 0)
-        <section id="patrons-adepts" style="grid-column: 1 / span 2">
+        <section class="sc-line">
+            <x-sc-scissors />
             <div class="section-header">
-                <h1><i class="fa-solid fa-chalkboard-user"></i> Potencjalni patroni</h1>
+                <h1>
+                    <i class="fa-solid fa-calendar"></i>
+                    Grafik
+                </h1>
                 <div>
-                    <x-a href="https://www.facebook.com/muzykaszytanamiarepl/reviews" target="_blank">Recenzje</x-a>
+                    <x-a href="{{ route('quests-calendar') }}">Wszystkie</x-a>
                 </div>
             </div>
-            <table>
-                <thead>
-                    <th>Klient</th>
-                    <th>Decyzja</th>
-                </thead>
-                <tbody>
-                    @foreach ($patrons_adepts as $patron)
-                    <tr>
-                        <td>
-                            <i class="fa-solid fa-{{ $patron->is_veteran ? 'user-shield' : 'user' }}"></i>
-                            <a href="{{ route('clients', ['search' => $patron->id]) }}">{{ _ct_($patron->client_name) }}</a>
-                        </td>
-                        <td>
-                            <x-button label="" icon="check" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 2]) }}" :small="true" />
-                            <x-button label="" icon="x" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 0]) }}" :small="true" />
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
+            <x-calendar :click-days="false" :suggest="false" :with-today="true" :length="7" />
+
+            <div class="section-header">
+                <h1><i class="fa-solid fa-envelope"></i> Zapytania</h1>
+                <div>
+                    <x-a href="{{ route('requests') }}">Wszystkie</x-a>
+                </div>
+            </div>
+            <style>
+            #dashboard-requests .table-row{ grid-template-columns: 3fr 2fr; }
+            .quest-type{ font-size: 1em; margin: 0; }
+            @media screen and (max-width: 600px){
+                #dashboard-requests .table-row{
+                    grid-template-columns: 1fr;
+                }
+            }
+            </style>
+            <div class="quests-table">
+                <div class="table-header table-row">
+                    <span>Utwór/Klient</span>
+                    <span>Meta</span>
+                </div>
+            @forelse ($requests as $request)
+            <a href="{{ route('request', $request->id) }}" class="table-row p-{{ $request->status_id }} {{ ($request->is_priority) ? "priority" : "" }}">
+                <span class="quest-main-data flex-down">
+                    <h3 class="song-title">{{ $request->title ?? "bez tytułu" }}</h3>
+                    @if($request->artist) <span class="song-artist">{{ $request->artist }}</span> @endif
+                    @if ($request->is_priority)
+                    <b>Priorytet</b>
+                    @endif
+                    <br>
+                    <span class="ghost">
+                        @if ($request->client?->client_name)
+                            @if ($request->client->is_veteran)
+                            <i class="fa-solid fa-user-shield" @popper(stały klient)></i> {{ _ct_($request->client->client_name) }}
+                            @else
+                            <i class="fa-solid fa-user" @popper(zwykły klient)></i> {{ _ct_($request->client->client_name) }}
+                            @endif
+                        @else
+                            <i class="fa-regular fa-user" @popper(nowy klient)></i> {{ _ct_($request->client_name) }}
+                        @endif
+                    </span>
+                </span>
+                <span>
+                    <span class="quest-status">
+                        <x-phase-indicator :status-id="$request->status_id" :small="true" />
+                    </span>
+                    <div class="quest-meta">
+                        @if ($request->price)
+                        <p>{{ _c_(as_pln($request->price)) }}</p>
+                        <i class="fa-solid fa-sack-dollar" @popper(Cena)></i>
+                        @endif
+
+                        @if ($request->hard_deadline)
+                        <p
+                            @if ($request->hard_deadline?->addDay()->subDays(1)->lte(now()))
+                            class="quest-deadline error"
+                            @elseif ($request->hard_deadline?->addDay()->subDays(3)->lte(now()))
+                            class="quest-deadline warning"
+                            @else
+                            class="quest-deadline"
+                            @endif
+                            {{ Popper::pop($request->hard_deadline->format("Y-m-d")) }} >
+                            {{ $request->hard_deadline?->addDay()->diffForHumans() }}
+                        </p>
+                        <i class="fa-solid fa-calendar-xmark" @popper(Termin od klienta)></i>
+                        @endif
+                        @if ($request->deadline)
+                        <p
+                            @if(in_array($request->status_id, [11, 12]))
+                                @if ($request->deadline?->addDay()->subDays(1)->lte(now()))
+                                class="quest-deadline error"
+                                @elseif ($request->deadline?->addDay()->subDays(3)->lte(now()))
+                                class="quest-deadline warning"
+                                @endif
+                            @else
+                                class="quest-deadline"
+                            @endif
+                            {{ Popper::pop($request->deadline->format("Y-m-d")) }} >
+                            {{ $request->deadline?->addDay()->diffForHumans() }}
+                        </p>
+                        <i class="fa-solid fa-calendar" @popper(Termin oddania pierwszej wersji)></i>
+                        @endif
+                    </div>
+                </span>
+            </a>
+            @empty
+                <p class="grayed-out">brak aktywnych zapytań</p>
+            @endforelse
+            </div>
         </section>
-        @endif
 
         @php $statuses_for_split = [11, 12, 13, 16, 26, 96]; @endphp
         @foreach([
@@ -254,110 +355,5 @@
             </div>
         </section>
         @endforeach
-
-        <section class="sc-line">
-            <x-sc-scissors />
-            <div class="section-header">
-                <h1>
-                    <i class="fa-solid fa-calendar"></i>
-                    Grafik
-                </h1>
-                <div>
-                    <x-a href="{{ route('quests-calendar') }}">Wszystkie</x-a>
-                </div>
-            </div>
-            <x-calendar :click-days="false" :suggest="false" :with-today="true" :length="7" />
-        </section>
-
-        <section id="dashboard-requests">
-            <div class="section-header">
-                <h1><i class="fa-solid fa-envelope"></i> Zapytania</h1>
-                <div>
-                    <x-a href="{{ route('requests') }}">Wszystkie</x-a>
-                </div>
-            </div>
-            <style>
-            #dashboard-requests .table-row{ grid-template-columns: 3fr 2fr; }
-            .quest-type{ font-size: 1em; margin: 0; }
-            @media screen and (max-width: 600px){
-                #dashboard-requests .table-row{
-                    grid-template-columns: 1fr;
-                }
-            }
-            </style>
-            <div class="quests-table">
-                <div class="table-header table-row">
-                    <span>Utwór/Klient</span>
-                    <span>Meta</span>
-                </div>
-            @forelse ($requests as $request)
-            <a href="{{ route('request', $request->id) }}" class="table-row p-{{ $request->status_id }} {{ ($request->is_priority) ? "priority" : "" }}">
-                <span class="quest-main-data flex-down">
-                    <h3 class="song-title">{{ $request->title ?? "bez tytułu" }}</h3>
-                    @if($request->artist) <span class="song-artist">{{ $request->artist }}</span> @endif
-                    @if ($request->is_priority)
-                    <b>Priorytet</b>
-                    @endif
-                    <br>
-                    <span class="ghost">
-                        @if ($request->client?->client_name)
-                            @if ($request->client->is_veteran)
-                            <i class="fa-solid fa-user-shield" @popper(stały klient)></i> {{ _ct_($request->client->client_name) }}
-                            @else
-                            <i class="fa-solid fa-user" @popper(zwykły klient)></i> {{ _ct_($request->client->client_name) }}
-                            @endif
-                        @else
-                            <i class="fa-regular fa-user" @popper(nowy klient)></i> {{ _ct_($request->client_name) }}
-                        @endif
-                    </span>
-                </span>
-                <span>
-                    <span class="quest-status">
-                        <x-phase-indicator :status-id="$request->status_id" :small="true" />
-                    </span>
-                    <div class="quest-meta">
-                        @if ($request->price)
-                        <p>{{ _c_(as_pln($request->price)) }}</p>
-                        <i class="fa-solid fa-sack-dollar" @popper(Cena)></i>
-                        @endif
-
-                        @if ($request->hard_deadline)
-                        <p
-                            @if ($request->hard_deadline?->addDay()->subDays(1)->lte(now()))
-                            class="quest-deadline error"
-                            @elseif ($request->hard_deadline?->addDay()->subDays(3)->lte(now()))
-                            class="quest-deadline warning"
-                            @else
-                            class="quest-deadline"
-                            @endif
-                            {{ Popper::pop($request->hard_deadline->format("Y-m-d")) }} >
-                            {{ $request->hard_deadline?->addDay()->diffForHumans() }}
-                        </p>
-                        <i class="fa-solid fa-calendar-xmark" @popper(Termin od klienta)></i>
-                        @endif
-                        @if ($request->deadline)
-                        <p
-                            @if(in_array($request->status_id, [11, 12]))
-                                @if ($request->deadline?->addDay()->subDays(1)->lte(now()))
-                                class="quest-deadline error"
-                                @elseif ($request->deadline?->addDay()->subDays(3)->lte(now()))
-                                class="quest-deadline warning"
-                                @endif
-                            @else
-                                class="quest-deadline"
-                            @endif
-                            {{ Popper::pop($request->deadline->format("Y-m-d")) }} >
-                            {{ $request->deadline?->addDay()->diffForHumans() }}
-                        </p>
-                        <i class="fa-solid fa-calendar" @popper(Termin oddania pierwszej wersji)></i>
-                        @endif
-                    </div>
-                </span>
-            </a>
-            @empty
-                <p class="grayed-out">brak aktywnych zapytań</p>
-            @endforelse
-            </div>
-        </section>
     </div>
 @endsection

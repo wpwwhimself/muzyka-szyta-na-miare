534ab08467626a71293656921ac3b8ecf7a9fdba
Wojciech Przybyła <contact@wpww.pl>
2023-12-15 22:46:50 +0100

    ✨ archmage - new request view

--- resources/views/archmage/request.blade.php
+++ resources/views/archmage/request.blade.php
@@ -36,25 +36,24 @@
     </div>
 
     @if ($request->quest_id)
-    <h2>
-        Zlecenie przepisane z numerem {{ $request->quest_id }}
-        <x-a href='{{ route("quest", ["id" => $request->quest_id]) }}'>Przejdź do zlecenia</x-a>
-    </h2>
+    <h2>Zlecenie przepisane z numerem <a href="{{ $request->quest->link_to }}">{{ $request->quest_id }}</a></h2>
     @endif
 
-    <div id="quest-box" class="flex-right">
-        <section class="input-group">
-            <h2>
-                <i class="fa-solid fa-user"></i>
-                Dane klienta
-                <a href="{{ route('clients', ['search' => $request->client?->id]) }}" @popper(szczegóły klienta) target="_blank" id="client_info"><i class="fa-solid fa-up-right-from-square"></i></a>
-            </h2>
+    <div class="flex-down spaced">
+        <x-extendo-block key="client"
+            header-icon="user"
+            title="Dane klienta"
+            :subtitle="$request->client_name"
+            :extended="in_array($request->status_id, [1])"
+        >
             <x-select name="client_id" label="Istniejący klient" :options="$clients" :empty-option="true" value="{{ $request->client_id }}" :small="true" />
             <x-input type="text" name="client_name" label="Nazwisko/Nazwa" :autofocus="true" :required="true" value="{{ _ct_($request->client_name) }}" />
             <x-input type="email" name="email" label="Adres e-mail" value="{{ _ct_($request->email) }}" :small="true" />
             <x-input type="tel" name="phone" label="Numer telefonu" value="{{ _ct_($request->phone) }}" :small="true" />
             <x-input type="text" name="other_medium" label="Inna forma kontaktu" value="{{ _ct_($request->other_medium) }}" :small="true" />
             <x-input type="text" name="contact_preference" label="Preferencja kontaktowa" placeholder="email" value="{{ _ct_($request->contact_preference) }}" />
+
+            <x-button action="{{ route('quests', ['client' => $request->client?->id]) }}" id="client_info" icon="boxes-stacked" label="Zlecenia" small />
 
             <script>
             function client_fields(dont_clear_fields = false){
@@ -88,7 +87,7 @@
                                     .off();
                             }
                             if(res.special_prices != null){$("#special-prices-warning").html(`<i class="fa-solid fa-triangle-exclamation"></i> Klient ma specjalną wycenę:<br>${res.special_prices}`);}
-                            $("#client_info").attr("href", "{{ route('clients') }}" + `?search=${$("#client_id").val()}`).show();
+                            $("#client_info").attr("href", "{{ route('quests') }}" + `?client=${$("#client_id").val()}`).show();
                         }
                     });
                 }else{
@@ -110,18 +109,30 @@
                 $("#client_id").change(function(){ client_fields() });
             });
             </script>
-        </section>
+        </x-extendo-block>
 
-        <section class="input-group">
-            <h2><i class="fa-solid fa-cart-flatbed"></i> Dane utworu</h2>
+        <x-extendo-block key="song"
+            header-icon="cart-flatbed"
+            title="Dane utworu"
+            :subtitle="$request->song?->full_title ?? $request->title"
+            :extended="in_array($request->status_id, [1])"
+            :warning="[
+                'Klient ma domyślne życzenia' => $request->client->default_wishes,
+                'Klient ma deadline' => $request->hard_deadline,
+            ]"
+        >
             <x-select name="song_id" label="Istniejący utwór" :options="$songs" :empty-option="true" :small="true" :value="$request->song_id" />
             <x-input type="text" name="title" label="Tytuł utworu" value="{{ $request->title }}" />
             <x-input type="text" name="artist" label="Wykonawca" value="{{ $request->artist }}" />
-            <x-input type="text" name="link" label="Link do nagrania" :small="true" value="{{ $request->link }}" />
-            <x-link-interpreter :raw="$request->link" />
+            <div>
+                <x-input type="text" name="link" label="Link do nagrania" :small="true" value="{{ $request->link }}" />
+                <x-link-interpreter :raw="$request->link" />
+            </div>
             <x-select name="genre_id" label="Gatunek" :options="$genres" :small="true" :empty-option="true" value="{{ $request->genre_id }}" :required="true" />
-            <x-input type="TEXT" name="wishes" label="Życzenia dotyczące utworu" value="{{ $request->wishes }}" />
+            <div>
+                <x-input type="TEXT" name="wishes" label="Życzenia dotyczące utworu" value="{{ $request->wishes }}" />
                 <x-button label="Wpisz domyślne życzenia" icon="circle-question" action="#/" id="default-wishes-button" :small="true" />
+            </div>
             <x-input type="TEXT" name="wishes_quest" label="Życzenia dotyczące zlecenia" value="{{ $request->wishes_quest }}" />
             <x-input type="date" name="hard_deadline" label="Termin narzucony przez klienta" value="{{ $request->hard_deadline?->format('Y-m-d') }}" />
 
@@ -166,122 +177,127 @@
                 });
             </script>
             @endif
-        </section>
+        </x-extendo-block>
 
-        <section class="input-group">
-            <h2><i class="fa-solid fa-sack-dollar"></i> Wycena</h2>
-
-            <h3>Kwota</h3>
+        <x-extendo-block key="quote"
+            header-icon="sack-dollar"
+            title="Wycena"
+            :extended="true"
+        >
             <x-select name="quest_type" label="Rodzaj zlecenia" :small="true" :options="$questTypes" :required="true" value="{{ $request->quest_type_id }}" />
-            <div id="song-price-sugg"></div>
-            <div id="special-prices-warning"></div>
-            <x-input type="text" name="price_code" label="Kod wyceny" :hint="$prices" value="{{ $request->price_code }}" />
-            <div id="price-summary" class="hint-table">
-                <div class="positions"></div>
-                <hr />
-                <div class="summary"><span>Razem:</span><span>0 zł</span></div>
+
+            <div>
+                <div id="song-price-sugg"></div>
+                <div id="special-prices-warning"></div>
+                <x-input type="text" name="price_code" label="Kod wyceny" :hint="$prices" value="{{ $request->price_code }}" />
+                <div id="price-summary" class="hint-table">
+                    <div class="positions"></div>
+                    <hr />
+                    <div class="summary"><span>Razem:</span><span>0 zł</span></div>
+                </div>
             </div>
-            <div id="delayed-payments-summary" class="hint-table">
-                <div class="positions"></div>
-                <hr />
-                <div class="summary"></div>
-            </div>
-            <script>
-            function calcPriceNow(){
-                const labels = $("#price_code").val();
-                const client_id = $("#client_id").val();
-                const positions_list = $("#price-summary .positions");
-                const sum_row = $("#price-summary .summary");
-                if(labels == "") positions_list.html(`<p class="grayed-out">podaj kategorie wyceny</p>`);
-                else{
+
+            <div>
+                <div id="delayed-payments-summary" class="hint-table">
+                    <div class="positions"></div>
+                    <hr />
+                    <div class="summary"></div>
+                </div>
+                <script>
+                function calcPriceNow(){
+                    const labels = $("#price_code").val();
+                    const client_id = $("#client_id").val();
+                    const positions_list = $("#price-summary .positions");
+                    const sum_row = $("#price-summary .summary");
+                    if(labels == "") positions_list.html(`<p class="grayed-out">podaj kategorie wyceny</p>`);
+                    else{
+                        $.ajax({
+                            url: "{{ url('price_calc') }}",
+                            type: "post",
+                            data: {
+                                _token: '{{ csrf_token() }}',
+                                labels: labels,
+                                client_id: client_id,
+                                quoting: true
+                            },
+                            success: function(res){
+                                let content = ``;
+                                for(line of res.positions){
+                                    content += `<span>${line[0]}</span><span>${line[1]}</span>`;
+                                }
+                                positions_list.html(content);
+                                sum_row.html(`<span>Razem:</span><span>${res.price} zł${res.minimal_price ? " (cena minimalna)" : ""}</span>`);
+                                if(res.override) positions_list.addClass("overridden");
+                                    else positions_list.removeClass("overridden");
+
+                                checkMonthlyPaymentLimit(res.price);
+                            }
+                        });
+                    }
+                }
+                function checkMonthlyPaymentLimit(price){
+                    const positions_list = $("#delayed-payments-summary .positions");
+                    const sum_row = $("#delayed-payments-summary .summary");
+
                     $.ajax({
-                        url: "{{ url('price_calc') }}",
+                        url: "{{ url('monthly_payment_limit') }}",
                         type: "post",
                         data: {
                             _token: '{{ csrf_token() }}',
-                            labels: labels,
-                            client_id: client_id,
-                            quoting: true
+                            amount: price,
                         },
                         success: function(res){
-                            let content = ``;
-                            for(line of res.positions){
-                                content += `<span>${line[0]}</span><span>${line[1]}</span>`;
+                            let when_to_ask = "";
+                            switch(res.when_to_ask){
+                                case 0: when_to_ask = "<span class='success'>od razu</span>"; break;
+                                case 1: when_to_ask = "<span class='warning'>w przyszłym miesiącu</span>"; break;
+                                default: when_to_ask = `<span class='error'>za ${res.when_to_ask} mc</span>`;
                             }
+                            let content = ``;
+                            content += `<span>Saturacja</span><span>${res.saturation[0]} zł • ${res.saturation[1]} zł • ${res.saturation[2]} zł</span>`;
                             positions_list.html(content);
-                            sum_row.html(`<span>Razem:</span><span>${res.price} zł${res.minimal_price ? " (cena minimalna)" : ""}</span>`);
-                            if(res.override) positions_list.addClass("overridden");
-                                else positions_list.removeClass("overridden");
+                            sum_row.html(`<span>Kiedy można brać?</span><span>${when_to_ask}</span>`);
 
-                            checkMonthlyPaymentLimit(res.price);
+                            let delayed_payment;
+                            if(res.when_to_ask == 0){
+                                delayed_payment = undefined;
+                            }else{
+                                let today = new Date();
+                                delayed_payment = (new Date(today.getFullYear(), today.getMonth() + res.when_to_ask, 1));
+                                delayed_payment = `${delayed_payment.getFullYear()}-${(delayed_payment.getMonth() + 1).toString().padStart(2, 0)}-${delayed_payment.getDate().toString().padStart(2, 0)}`;
+                            }
+                            document.getElementById("delayed_payment").value = delayed_payment;
                         }
                     });
                 }
-            }
-            function checkMonthlyPaymentLimit(price){
-                const positions_list = $("#delayed-payments-summary .positions");
-                const sum_row = $("#delayed-payments-summary .summary");
-
-                $.ajax({
-                    url: "{{ url('monthly_payment_limit') }}",
-                    type: "post",
-                    data: {
-                        _token: '{{ csrf_token() }}',
-                        amount: price,
-                    },
-                    success: function(res){
-                        let when_to_ask = "";
-                        switch(res.when_to_ask){
-                            case 0: when_to_ask = "<span class='success'>od razu</span>"; break;
-                            case 1: when_to_ask = "<span class='warning'>w przyszłym miesiącu</span>"; break;
-                            default: when_to_ask = `<span class='error'>za ${res.when_to_ask} mc</span>`;
-                        }
-                        let content = ``;
-                        content += `<span>Saturacja</span><span>${res.saturation[0]} zł • ${res.saturation[1]} zł • ${res.saturation[2]} zł</span>`;
-                        positions_list.html(content);
-                        sum_row.html(`<span>Kiedy można brać?</span><span>${when_to_ask}</span>`);
-
-                        let delayed_payment;
-                        if(res.when_to_ask == 0){
-                            delayed_payment = undefined;
-                        }else{
-                            let today = new Date();
-                            delayed_payment = (new Date(today.getFullYear(), today.getMonth() + res.when_to_ask, 1));
-                            delayed_payment = `${delayed_payment.getFullYear()}-${(delayed_payment.getMonth() + 1).toString().padStart(2, 0)}-${delayed_payment.getDate().toString().padStart(2, 0)}`;
-                        }
-                        document.getElementById("delayed_payment").value = delayed_payment;
-                    }
+                $(document).ready(function(){
+                    calcPriceNow();
+                    $("#price_code").change(function (e) { calcPriceNow() });
                 });
-            }
-            $(document).ready(function(){
-                calcPriceNow();
-                $("#price_code").change(function (e) { calcPriceNow() });
-            });
-            </script>
-            @if ($request->client?->budget && in_array($request->status_id, [1, 5, 6]))
-            <span class="{{ $request->client->budget >= $request->price ? 'success' : 'warning' }}">
-                <i class="fa-solid fa-sack-dollar"></i>
-                Budżet w wysokości <b>{{ as_pln($request->client->budget) }}</b> automatycznie
-                <br>
-                pokryje
-                @if ($request->client->budget >= $request->price)
-                całą kwotę zlecenia
-                @else
-                część kwoty zlecenia
+                </script>
+                @if ($request->client?->budget && in_array($request->status_id, [1, 5, 6]))
+                <span class="{{ $request->client->budget >= $request->price ? 'success' : 'warning' }}">
+                    <i class="fa-solid fa-sack-dollar"></i>
+                    Budżet w wysokości <b>{{ as_pln($request->client->budget) }}</b> automatycznie
+                    <br>
+                    pokryje
+                    @if ($request->client->budget >= $request->price)
+                    całą kwotę zlecenia
+                    @else
+                    część kwoty zlecenia
+                    @endif
+                </span>
                 @endif
-            </span>
-            @endif
-            <x-input type="date" name="delayed_payment" label="Opóźnienie wpłaty" value="{{ $request->delayed_payment?->format('Y-m-d') }}" />
+                <x-input type="date" name="delayed_payment" label="Opóźnienie wpłaty" value="{{ $request->delayed_payment?->format('Y-m-d') }}" />
+            </div>
 
-            <h3>Termin</h3>
-            @if (in_array($request->status_id, [1, 6, 96])) <x-calendar /> @endif
-            <x-input type="date" name="deadline" label="Termin oddania pierwszej wersji" value="{{ $request->deadline?->format('Y-m-d') }}" />
-        </section>
+            <div>
+                @if (in_array($request->status_id, [1, 6, 96])) <x-calendar /> @endif
+                <x-input type="date" name="deadline" label="Termin oddania pierwszej wersji" value="{{ $request->deadline?->format('Y-m-d') }}" />
+            </div>
+        </x-extendo-block>
 
-        <section class="input-group">
-            <h2><i class="fa-solid fa-timeline"></i> Historia</h2>
-            <x-quest-history :quest="$request" />
-        </section>
+        <x-quest-history :quest="$request" />
     </div>
 </form>
 

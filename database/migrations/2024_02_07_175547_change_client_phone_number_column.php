077f29e95e731f9441407ed36c6734aa664f6806
Wojciech Przybyła <contact@wpww.pl>
2024-02-07 18:21:37 +0100

    🚑 phone number is too short!!!

---
+++ database/migrations/2024_02_07_175547_change_client_phone_number_column.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeClientPhoneNumberColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table("clients", function (Blueprint $table) {
+            $table->string("phone", 20)->nullable()->change();
+        });
+        Schema::table("requests", function (Blueprint $table) {
+            $table->string("phone", 20)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table("clients", function (Blueprint $table) {
+            $table->integer("phone")->nullable()->change();
+        });
+        Schema::table("requests", function (Blueprint $table) {
+            $table->integer("phone")->nullable()->change();
+        });
+    }
+}

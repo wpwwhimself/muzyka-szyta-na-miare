42395df5f6041d0ee54c34a533c439b07a4edc07
Wojciech Przybyła <contact@wpww.pl>
2024-04-29 12:04:57 +0200

    🚑 restoring broken animations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,37 @@
 require('./bootstrap');
+
+/**
+ * Ukrywanie alertów
+ */
+const TOAST_TIMEOUT = 4000;
+if($(".alert").length){ //if exists
+    //appear
+    setTimeout(() => {
+        $(".alert").addClass("in");
+    }, 1);
+
+    //allow dismissal
+    $(".alert").click(() => $(".alert").removeClass("in"));
+
+    //disappear
+    setTimeout(() => {
+        $(".alert").removeClass("in");
+    }, TOAST_TIMEOUT);
+}
+/**
+ * Niebezpieczne guziki
+ */
+function dangerConfirm(){
+    let x = confirm("Ostrożnie! Czy na pewno chcesz to zrobić?");
+    if(!x){
+        event.preventDefault();
+    }
+}
+/**
+ * Wyłacznik tutoriali
+ */
+$(document).ready(()=>{
+    if(IS_VETERAN){
+        $(".tutorial").hide();
+    }
+});

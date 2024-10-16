321245b5e1e0519b972c995e1897394169cdc704
Wojciech Przybyła <contact@wpww.pl>
2024-10-16 14:16:39 +0200

    🚸 request | client immediately sees all price info upon receiving quote

--- resources/views/client/request.blade.php
+++ resources/views/client/request.blade.php
@@ -81,7 +81,7 @@
                 $request->deadline ? 'do '.$request->deadline->format('d.m.Y') : null,
             ], fn($val) => !is_null($val)))"
             :warning="$warnings['quote']"
-            :extended="sumWarnings($warnings['quote'], true)"
+            :extended="sumWarnings($warnings['quote'], true) || $request->status_id == 5"
         >
             @if (!$request->price)
             <p class="yellowed-out"><i class="fa-solid fa-hourglass-half fa-fade"></i> pojawi się w ciągu najbliższych dni</p>

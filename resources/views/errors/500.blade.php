ea1e6f5a22e5de4fe535db9904decd5963d97472
Wojciech Przybyła <contact@wpww.pl>
2025-01-13 17:49:07 +0100

    🚸 added hint for clients encountering 500s

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -4,6 +4,7 @@
 
 <div id="error-page">
     <h1 class="error">500</h1>
-    <p>Coś złego stało się na serwerze. To nie Twoja wina</p>
+    <p>Coś złego stało się na serwerze. To nie Twoja wina.</p>
+    <p class="yellowed-out"><a href="mailto:{{ env("MAIL_MAIN_ADDRESS") }}">Wyślij do mnie maila</a> z informacją, że błąd występuje i jaka operacja była wykonywana tuż przed pojawieniem się tego błędu.</p>
 </div>
 @endsection

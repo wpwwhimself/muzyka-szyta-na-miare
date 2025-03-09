747fbbce7da75b34446bb9990e36c392bf5aa8d4
Wojciech Przybyła <102858173+wpwwhimself@users.noreply.github.com>
2025-03-09 10:57:40 +0100

    Aktualisieren von description.blade.php

--- resources/views/components/showcases/description.blade.php
+++ resources/views/components/showcases/description.blade.php
@@ -3,11 +3,44 @@
 ])
 
 <code onclick="copyDesc(this)">
+@switch ($for)
+@case ("podklady")
+$tytuł$ ($wykonawca$) | Jak się szyje muzykę
 
+Krótkie demo, jak powstała partia $partia$ do mojego podkładu. $flavor text$
+
+Zobacz więcej rolek:
+🎵 https://www.tiktok.com/@muzykaszytanamiarepl
+▶️ https://www.youtube.com/@muzykaszytanamiarepl
+📷 https://www.instagram.com/muzykaszytanamiarepl
+
+Moje usługi:
+✂️ https://muzykaszytanamiare.pl/
+
+#muzykaszytanamiarę #podkład #karaoke #cover
+#$tytuł$ #$autor$ #$gatunek$ #$instrument$
+@break
+
+@case ("organista")
+$tytuł$ | Shorty organisty
+
+Zobacz więcej rolek:
+🎵 https://www.tiktok.com/@muzykaszytanamiarepl
+▶️ https://www.youtube.com/@muzykaszytanamiarepl
+📷 https://www.instagram.com/muzykaszytanamiarepl
+
+Moje usługi:
+✂️ https://muzykaszytanamiare.pl/
+
+#muzykaszytanamiarę #organy #msza
+#$pieśńczypsalm$
+@break
+@endswitch
 </code>
 
 <script>
 function copyDesc(el) {
+    navigator.clipboard.writeText(el.textContent)
     alert("Opis skopiowany")
 }
 </script>

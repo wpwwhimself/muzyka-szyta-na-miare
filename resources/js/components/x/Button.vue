a138f598503fc18cb91ad38a739e15b4db484905
Wojciech Przybyła <contact@wpww.pl>
2025-03-13 14:25:21 +0100

    🪟 dj | improved icons on buttons

--- resources/js/components/x/Button.vue
+++ resources/js/components/x/Button.vue
@@ -1,10 +1,13 @@
 <script setup>
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faAnglesLeft } from '@fortawesome/free-solid-svg-icons';
+
 defineProps(["icon"])
 </script>
 
 <template>
     <div class="submit small">
-        <FontAwesomeIcon v-if="icon" :icon="icon" />
+        <FontAwesomeIcon :icon="icon || faAnglesLeft" />
 
         <slot />
     </div>

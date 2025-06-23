771fb2fe537a927682a650999b1c3edcdfe64408
Wojciech Przybyła <contact@wpww.pl>
2025-06-23 15:12:42 +0200

    🚸 gig-price | added clarifications

--- resources/views/archmage/gig-price/suggest.blade.php
+++ resources/views/archmage/gig-price/suggest.blade.php
@@ -88,12 +88,12 @@ function calculateGigPrice() {
         "value": 2 * settings.travel_distance_km,
     }
     calculations["distance_cost"] = {
-        "label": "Koszt przejazdu",
+        "label": "Koszt przejazdu (w jedną stronę)",
         "unit": "zł",
         "value": settings.fuel_cost_pln_per_l * settings.fuel_consumption_l_per_100_km / 100 * calculations.distance_traveled.value,
     }
     calculations["passive_cost"] = {
-        "label": "Koszt pasywny (dojazd, czekanie)",
+        "label": "Koszt pasywny (dojazd łącznie, czekanie)",
         "unit": "zł",
         "value": settings.gain_passive_per_h * (2 * calculations.drive_time.value + settings.gig_time_buffer_h),
     }

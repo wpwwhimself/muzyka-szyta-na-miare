2ff089e11d5f76d37943e1669ed9b6a188cac21b
Wojciech Przybyła <contact@wpww.pl>
2025-06-25 18:51:43 +0200

    ♻️ dashboard | updating dashboard to be more functional

--- resources/views/archmage/dashboard.blade.php
+++ resources/views/archmage/dashboard.blade.php
@@ -2,38 +2,123 @@
 
 @section('content')
 
+@if (count($patrons_adepts) > 0)
+<x-section id="patrons-adepts"
+    title="Potencjalni patroni"
+    icon="chalkboard-user"
+    style="grid-column: 1 / span 2;"
+>
+    <x-slot name="buttons">
+        <x-a href="https://www.facebook.com/muzykaszytanamiarepl/reviews" target="_blank">Recenzje</x-a>
+    </x-slot>
+
+    <table>
+        <thead>
+            <th>Klient</th>
+            <th>Decyzja</th>
+        </thead>
+        <tbody>
+            @foreach ($patrons_adepts as $patron)
+            <tr>
+                <td>
+                    <a href="{{ route('clients', ['search' => $patron->id]) }}">{!! $patron !!}</a>
+                </td>
+                <td>
+                    <x-button label="" icon="check" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 2]) }}" :small="true" />
+                    <x-button label="" icon="x" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 0]) }}" :small="true" />
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</x-section>
+@endif
+
+<x-section title="Zapytania" icon="envelope">
+    <x-slot name="buttons">
+        <x-a href="{{ route('add-request') }}" icon="plus">Dodaj nowe</x-a>
+        <x-a href="{{ route('requests') }}">Wszystkie</x-a>
+    </x-slot>
+
+    @forelse ($requests as $request)
+    <x-quest-mini :quest="$request" />
+    @empty
+    <p class="grayed-out"><i class="fas fa-check"></i> brak aktywnych zapytań</p>
+    @endforelse
+</x-section>
+
+@if (count($showcases_missing))
+<x-section title="Showcase'y do stworzenia" icon="bullhorn" style="grid-column: 1 / span 2;">
+    <table>
+        <thead>
+            <tr>
+                <th><i class="fas fa-box"></i> ID</th>
+                <th><i class="fas fa-compact-disc"></i> ID</th>
+                <th>Utwór</th>
+                <th>Co trzeba zrobić</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($showcases_missing as $quest)
+            <tr>
+                <td><a href="{{ route('quest', ['id' => $quest->id]) }}">{{ $quest->id }}</a></td>
+                <td><a href="{{ route('song-edit', ['id' => $quest->song->id]) }}">{{ $quest->song->id }}</a></td>
+                <td>{{ $quest->song->full_title }}</td>
+                <td>
+                    @if ($quest->song->has_recorded_reel)
+                        @if ($quest->song->has_original_mv)
+                        <i class="fas fa-photo-film" @popper(Rolka z teledyskiem)></i>
+                        @else
+                        <i class="fas fa-film" @popper(Rolka)></i>
+                        @endif
+                    @endif
+
+                    @if (!$quest->song->has_showcase_file)
+                    <i class="fas fa-shirt" @popper(Krótki showcase)></i>
+                    @endif
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</x-section>
+@endif
+
+<x-section id="dashboard-quests"
+    title="Zlecenia w toku"
+    icon="box"
+>
+    @forelse ($quests_ongoing as $key => $quest)
+    <x-quest-mini :quest="$quest" :no="$key + 1" />
+    @empty
+    <p class="grayed-out">brak aktywnych zleceń</p>
+    @endforelse
+</x-section>
+
 <div class="grid-2">
-    @if (count($patrons_adepts) > 0)
-    <x-section id="patrons-adepts"
-        title="Potencjalni patroni"
-        icon="chalkboard-user"
-        style="grid-column: 1 / span 2;"
+    <x-section id="dashboard-requests" class="sc-line"
+        title="Grafik"
+        icon="calendar"
     >
+        <x-sc-scissors />
+
         <x-slot name="buttons">
-            <x-a href="https://www.facebook.com/muzykaszytanamiarepl/reviews" target="_blank">Recenzje</x-a>
+            <x-a href="{{ route('quests-calendar') }}">Wszystkie</x-a>
         </x-slot>
 
-        <table>
-            <thead>
-                <th>Klient</th>
-                <th>Decyzja</th>
-            </thead>
-            <tbody>
-                @foreach ($patrons_adepts as $patron)
-                <tr>
-                    <td>
-                        <a href="{{ route('clients', ['search' => $patron->id]) }}">{!! $patron !!}</a>
-                    </td>
-                    <td>
-                        <x-button label="" icon="check" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 2]) }}" :small="true" />
-                        <x-button label="" icon="x" action="{{ route('patron-mode', ['client_id' => $patron->id, 'level' => 0]) }}" :small="true" />
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <x-calendar :click-days="false" :suggest="false" :with-today="true" />
     </x-section>
-    @endif
+
+    <x-section id="dashboard-quests"
+        title="Zlecenia czekające"
+        icon="box-open"
+    >
+        @forelse ($quests_review as $key => $quest)
+        <x-quest-mini :quest="$quest" :no="$key + 1" />
+        @empty
+        <p class="grayed-out">brak aktywnych zleceń</p>
+        @endforelse
+    </x-section>
 
     <x-section id="recent"
         title="Ostatnie zmiany"
@@ -150,85 +235,6 @@
             </tbody>
         </table>
     </x-section>
-
-    <x-section id="dashboard-requests" class="sc-line"
-        title="Grafik"
-        icon="calendar"
-    >
-        <x-sc-scissors />
-
-        <x-slot name="buttons">
-            <x-a href="{{ route('quests-calendar') }}">Wszystkie</x-a>
-        </x-slot>
-
-        <x-calendar :click-days="false" :suggest="false" :with-today="true" />
-    </x-section>
-
-    <x-section title="Zapytania" icon="envelope">
-        <x-slot name="buttons">
-            <x-a href="{{ route('add-request') }}" icon="plus">Dodaj nowe</x-a>
-            <x-a href="{{ route('requests') }}">Wszystkie</x-a>
-        </x-slot>
-
-        @forelse ($requests as $request)
-        <x-quest-mini :quest="$request" />
-        @empty
-        <p class="grayed-out">brak aktywnych zapytań</p>
-        @endforelse
-    </x-section>
-
-    @if (count($showcases_missing))
-    <x-section title="Showcase'y do stworzenia" icon="bullhorn" style="grid-column: 1 / span 2;">
-        <table>
-            <thead>
-                <tr>
-                    <th><i class="fas fa-box"></i> ID</th>
-                    <th><i class="fas fa-compact-disc"></i> ID</th>
-                    <th>Utwór</th>
-                    <th>Co trzeba zrobić</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($showcases_missing as $quest)
-                <tr>
-                    <td><a href="{{ route('quest', ['id' => $quest->id]) }}">{{ $quest->id }}</a></td>
-                    <td><a href="{{ route('song-edit', ['id' => $quest->song->id]) }}">{{ $quest->song->id }}</a></td>
-                    <td>{{ $quest->song->full_title }}</td>
-                    <td>
-                        @if ($quest->song->has_recorded_reel)
-                            @if ($quest->song->has_original_mv)
-                            <i class="fas fa-photo-film" @popper(Rolka z teledyskiem)></i>
-                            @else
-                            <i class="fas fa-film" @popper(Rolka)></i>
-                            @endif
-                        @endif
-
-                        @if (!$quest->song->has_showcase_file)
-                        <i class="fas fa-shirt" @popper(Krótki showcase)></i>
-                        @endif
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    </x-section>
-    @endif
-
-    @foreach([
-        ["Zlecenia w toku", "box", $quests_ongoing],
-        ["Zlecenia czekające", "box-open", $quests_review],
-    ] as [$sec_title, $icon, $data])
-    <x-section id="dashboard-quests"
-        :title="$sec_title"
-        :icon="$icon"
-    >
-        @forelse ($data as $key => $quest)
-        <x-quest-mini :quest="$quest" :no="$key + 1" />
-        @empty
-        <p class="grayed-out">brak aktywnych zleceń</p>
-        @endforelse
-    </x-section>
-    @endforeach
 </div>
 
 @endsection

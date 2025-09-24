44f80644f1155eb568d2bc70719dfa9ee629e081
Wojciech Przybyła <contact@wpww.pl>
2025-09-24 19:49:46 +0200

    🚧 started rebuilding for shipyard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,64 +1,30 @@
-<!DOCTYPE html>
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends("layouts.shipyard.admin")
 
-    <link rel="icon" href="{{ asset("msznm.svg") }}">
-    <link rel="stylesheet" href="{{ asset("css/app.css") }}?{{ time() }}">
-    @if (isset($extraCss))
-    <link rel="stylesheet" href="{{ asset("css/$extraCss.css") }}">
-    @endif
+@section("prepends")
+<script>
+const USER_ID = {{ Auth::id() ?? 'null' }};
+const IS_VETERAN = {{ is_archmage() ? 0 : intval(Auth::user()?->is_veteran ?? "") }};
+</script>
+@endsection
 
-    <script src="https://kit.fontawesome.com/97bfe258ce.js" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-    <script src="{{ asset("js/app.js") }}?{{ time() }}"></script>
+@section("appends")
+<div id="background-division">
+    @for ($i = 0; $i < 2; $i++)
+    <img
+        src="{{ asset("assets/divisions/"
+            .($i == 0 ? (Str::between(Request::root(), "://", ".".env("APP_DOMAIN")) ?? "msznm") : "msznm")
+            .".svg") }}"
+        alt="division logo"
+        class="white-on-black"
+    >
+    @endfor
+</div>
+@endsection
 
-    <script>
-    const USER_ID = {{ Auth::id() ?? 'null' }};
-    const IS_VETERAN = {{ is_archmage() ? 0 : intval(Auth::user()?->is_veteran ?? "") }};
-    </script>
+@hasSection("sidebar")
+@yield("sidebar")
+@endif
 
-    @env("local")
-    <style>
-    :root{
-        --acc: rgb(235, 34, 235) !important;
-    }
-    </style>
-    @endenv
-
-    <title>{{ $title != null ? "$title | " : "" }}{{ config("app.name") }}</title>
-</head>
-<body>
-    <x-header :title="$title" :stripped="$stripped ?? false" />
-
-    <div id="background-division">
-        @for ($i = 0; $i < 2; $i++)
-        <img
-            src="{{ asset("assets/divisions/"
-                .($i == 0 ? (Str::between(Request::root(), "://", ".".env("APP_DOMAIN")) ?? "msznm") : "msznm")
-                .".svg") }}"
-            alt="division logo"
-            class="white-on-black"
-        >
-        @endfor
-    </div>
-
-    @foreach (["success", "error"] as $status)
-        @if (session($status))
-            <x-alert :status="$status" />
-        @endif
-    @endforeach
-
-    <div class="main-wrapper">
-        @yield("content")
-    </div>
-
-    <x-footer :stripped="$stripped ?? false" />
-
-    @include("popper::assets")
-</body>
-</html>
+@hasSection("content")
+@yield("content")
+@endif

d13e431086f11dc46374b40e77d14c8530337ca9
Wojciech Przybyła <contact@wpww.pl>
2025-09-29 12:04:43 +0200

    🔐 fixed role verification for guests

--- app/helpers.php
+++ app/helpers.php
@@ -78,7 +78,7 @@ if(!function_exists("user_role")){
 }
 if(!function_exists("is_archmage")){
     function is_archmage($user_id = null){
-        return User::find($user_id ?? Auth::id())->hasRole("archmage");
+        return User::find($user_id ?? Auth::id())?->hasRole("archmage") ?? false;
     }
 }
 

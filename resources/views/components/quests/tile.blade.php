10f66ffbfef8d3a80f428530cb48330d60627077
Wojciech Przybyła <contact@wpww.pl>
2025-10-23 20:52:05 +0200

    🪗 added quest queue numbers

--- resources/views/components/quests/tile.blade.php
+++ resources/views/components/quests/tile.blade.php
@@ -1,9 +1,19 @@
 @props([
-    "quest"
+    "quest",
+    "no" => null,
 ])
 
 <x-shipyard.app.model.tile :model="$quest">
     <x-slot:actions>
+        @if ($no)
+        <x-shipyard.app.icon-label-value
+            icon="sort-reverse-variant"
+            label="Numer zlecenia w kolejce"
+        >
+            {{ $no }}
+        </x-shipyard.app.icon-label-value>
+        @endif
+
         <div class="quest-status">
             <x-phase-indicator :status-id="$quest->status_id" :small="true" />
         </div>

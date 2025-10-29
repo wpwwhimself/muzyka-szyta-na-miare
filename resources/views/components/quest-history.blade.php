c9ff3af1089ce33a49525563ba1c8ba958ed5448
Wojciech Przybyła <contact@wpww.pl>
2025-10-29 13:01:49 +0100

    ♻️ history | readded mail counters

--- resources/views/components/quest-history.blade.php
+++ resources/views/components/quest-history.blade.php
@@ -34,7 +34,8 @@
             "history-position",
             "flex",
             "down",
-            "center",
+            "middle",
+            "no-gap",
         ])>
             <div @class([
                 "circle",
@@ -51,15 +52,18 @@
             <small class="notification-counter">
                 @if ($item->mail_sent >= 1)
                     @for ($i = 0; $i < $item->mail_sent; $i++)
-                    <i class="fa-solid fa-envelope-circle-check" @popper(Mail wysłany)></i>
+                    <x-shipyard.app.icon name="email-fast" />
                     @endfor
                 @elseif ($item->mail_sent === 0)
-                <i class="fa-solid fa-comment" @popper(Wiadomość wysłana pozamailowo)></i>
+                <x-shipyard.app.icon name="message-fast" />
                 @endif
             </small>
             @if (is_archmage() && !is_archmage($item->changed_by))
-            <a @popper(Przypięcie komentarza) href="{{ route('showcase-pin-comment', ['comment_id' => $item->id, 'client_id' => $item->changed_by]) }}">
-                <i class="fa-solid fa-thumbtack {{ $item->pinned ? 'accent' : 'ghost' }}"></i>
+            <a @popper(Przypięcie komentarza)
+                href="{{ route('showcase-pin-comment', ['comment_id' => $item->id, 'client_id' => $item->changed_by]) }}"
+                class="{{ $item->pinned ? 'accent primary' : 'ghost' }}"
+            >
+                <x-shipyard.app.icon name="pin" />
             </a>
             @endif
         </div>

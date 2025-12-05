b92ab171a62239355951bbc85788d37b42577d1c
Wojciech Przybyła <contact@wpww.pl>
2025-12-05 13:43:41 +0100

    ⚓ refreshed sections

--- resources/views/components/extendo-block.blade.php
+++ resources/views/components/extendo-block.blade.php
@@ -1,57 +1,37 @@
-<div {{ $attributes->class([
-    "extendo-block",
-    "section",
-    "warning-like" => $warning,
-    "sc-line" => $scissors,
-]) }} data-ebid="{{ $key }}">
+<x-shipyard.app.section
+    :title="$title"
+    :subtitle="$subtitle"
+    :icon="$headerIcon"
+    :extended="$extended"
+    @class([
+        "warning-like" => $warning,
+        "sc-line" => $scissors,
+    ])
+    {{ $attributes }}
+>
+    <x-slot:actions>
+        @if ($warning)
+        @php
+            $warning_content = [];
+            foreach ($warning as $message => $test) {
+                $warning_content[] = $message;
+            }
+        @endphp
+        <span class="accent danger"
+            {{ Popper::arrow()->pop(Illuminate\Mail\Markdown::parse(implode("<br>", $warning_content))) }}
+        >
+            <x-shipyard.app.icon name="alert" />
+        </span>
+        @endif
+
+        @isset ($buttons)
+        {{ $buttons }}
+        @endisset
+    </x-slot:actions>
+
+    @isset ($slot)
+    {{ $slot }}
+    @endisset
+
     @if($scissors) <x-sc-scissors /> @endif
-
-    <div class="header flex right keep-for-mobile nowrap">
-        <div class="titles flex right keep-for-mobile">
-            <h2><x-shipyard.app.icon :name="$headerIcon" /></h2>
-            <h2>{{ $title }}</h2>
-            <span class="ghost">{!! $subtitle !!}</span>
-        </div>
-
-        <div class="right-side flex right middle keep-for-mobile">
-            @if ($warning)
-            @php
-                $warning_content = [];
-                foreach ($warning as $message => $test) {
-                    $warning_content[] = $message;
-                }
-            @endphp
-            <span class="accent danger"
-                {{ Popper::arrow()->pop(Illuminate\Mail\Markdown::parse(implode("<br>", $warning_content))) }}
-            >
-                <x-shipyard.app.icon name="alert" />
-            </span>
-            @endif
-
-            @if($buttons)
-            {{ $buttons }}
-            @endif
-
-            @unless($extended === "perma")
-            <x-shipyard.ui.button
-                icon="unfold-less-horizontal"
-                pop="Zwiń"
-                action="none"
-                onclick="openExtendoBlock(this, '{{ $key }}')"
-                class="toggles tertiary {{ $extended ? '' : 'hidden' }}"
-            />
-            <x-shipyard.ui.button
-                icon="unfold-more-horizontal"
-                pop="Rozwiń"
-                action="none"
-                onclick="openExtendoBlock(this, '{{ $key }}')"
-                class="toggles tertiary {{ $extended ? 'hidden' : '' }}"
-            />
-            @endunless
-        </div>
-    </div>
-
-    <div @class(['body', 'hidden' => !$extended])>
-        {{ $slot }}
-    </div>
-</div>
+</x-shipyard.app.section>
--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -13,62 +13,40 @@ $key ??= Str::uuid();
 $warning = array_filter($warning ?? []);
 @endphp
 
-<div {{ $attributes->class([
-    "extendo-block",
-    "section",
-    "warning-like" => $warning,
-    "sc-line" => $scissors,
-]) }} data-ebid="{{ $key }}">
-    @if ($title)
-    <div class="header flex right keep-for-mobile nowrap">
-        <div class="titles flex right keep-for-mobile">
-            <h2><x-shipyard.app.icon :name="$icon" /></h2>
-            <h2>{{ $title }}</h2>
-            <span class="ghost">{!! $subtitle !!}</span>
-        </div>
+<x-shipyard.app.section
+    :title="$title"
+    :subtitle="$subtitle"
+    :icon="$icon"
+    :extended="$extended"
+    @class([
+        "warning-like" => !!$warning,
+        "sc-line" => $scissors,
+    ])
+    {{ $attributes }}
+>
+    <x-slot:actions>
+        @if ($warning)
+        @php
+            $warning_content = [];
+            foreach ($warning as $message => $test) {
+                $warning_content[] = $message;
+            }
+        @endphp
+        <span class="accent danger"
+            {{ Popper::arrow()->pop(Illuminate\Mail\Markdown::parse(implode("<br>", $warning_content))) }}
+        >
+            <x-shipyard.app.icon name="alert" />
+        </span>
+        @endif
 
-        <div class="right-side flex right middle keep-for-mobile">
-            @if ($warning)
-            @php
-                $warning_content = [];
-                foreach ($warning as $message => $test) {
-                    $warning_content[] = $message;
-                }
-            @endphp
-            <span class="accent danger"
-                {{ Popper::arrow()->pop(Illuminate\Mail\Markdown::parse(implode("<br>", $warning_content))) }}
-            >
-                <x-shipyard.app.icon name="alert" />
-            </span>
-            @endif
+        @isset ($buttons)
+        {{ $buttons }}
+        @endisset
+    </x-slot:actions>
 
-            @isset ($buttons)
-            {{ $buttons }}
-            @endisset
-
-            @unless($extended === "perma")
-            <x-shipyard.ui.button
-                icon="unfold-less-horizontal"
-                pop="Zwiń"
-                action="none"
-                onclick="openExtendoBlock(this, '{{ $key }}')"
-                class="toggles tertiary {{ $extended ? '' : 'hidden' }}"
-            />
-            <x-shipyard.ui.button
-                icon="unfold-more-horizontal"
-                pop="Rozwiń"
-                action="none"
-                onclick="openExtendoBlock(this, '{{ $key }}')"
-                class="toggles tertiary {{ $extended ? 'hidden' : '' }}"
-            />
-            @endunless
-        </div>
-    </div>
-    @endif
-
-    <div @class(['body', 'hidden' => !$extended])>
-        {{ $slot }}
-    </div>
+    @isset ($slot)
+    {{ $slot }}
+    @endisset
 
     @if ($scissors) <x-sc-scissors /> @endif
-</div>
+</x-shipyard.app.section>

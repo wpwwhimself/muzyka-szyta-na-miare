219745375d475747c9c02b6f13ea22f8b061d8eb
Wojciech Przybyła <102858173+wpwwhimself@users.noreply.github.com>
2025-12-07 11:33:22 +0100

    🚸 added toasts to login

--- routes/web.php
+++ routes/web.php
@@ -34,7 +34,7 @@ Route::controller(HomeController::class)->group(function(){
     Route::get("catalog", "catalog")->name("catalog");
 });
 
-Route::redirect("/profile", "/dashboard");
+Route::redirect("/profile", "/dashboard")->withInput();
 
 Route::middleware("auth")->group(function(){
     Route::controller(BackController::class)->group(function(){

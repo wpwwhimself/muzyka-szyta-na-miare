ceaf60339a67c67a93957fb237a3af82254121a3
Wojciech Przybyła <contact@wpww.pl>
2026-02-07 21:38:47 +0100

    🩹 showcase reels ready for normal youtube videos

--- resources/views/components/front/showcase-reels.blade.php
+++ resources/views/components/front/showcase-reels.blade.php
@@ -7,7 +7,7 @@
     @forelse ($showcases as $showcase)
         @switch($showcase->platform)
             @case("yt")
-                <iframe width="{{ $player_dims[0] }}" height="{{ $player_dims[1] }}" src="https://www.youtube.com/embed/{{ Str::between($showcase->link, "shorts/", "?") }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe width="{{ $player_dims[0] }}" height="{{ $player_dims[1] }}" src="https://www.youtube.com/embed/{{ Str::contains($showcase->link, "shorts") ? Str::between($showcase->link, "shorts/", "?") : Str::after($showcase->link, "watch?v=") }}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
                 @break
             @case("tt")
                 <iframe src="https://www.tiktok.com/player/v1/{{ Str::between($showcase->link, "video/", "?") }}" width="{{ $player_dims[0] }}" height="{{ $player_dims[1] }}" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowfullscreen></iframe>

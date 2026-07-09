8bff9b2210e2881b0fe87c5fa83bd895a42277e5
Wojciech Przybyła <contact@wpww.pl>
2026-07-09 21:58:53 +0200

    ⚓ shipyardified models

---
+++ app/Models/NavItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Shipyard\NavItem as ShipyardNavItem;
+
+class NavItem extends ShipyardNavItem
+{
+    public const FROM_SHIPYARD = true;
+
+}
---
+++ app/Models/StandardPage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Shipyard\StandardPage as ShipyardStandardPage;
+
+class StandardPage extends ShipyardStandardPage
+{
+    public const FROM_SHIPYARD = true;
+
+}

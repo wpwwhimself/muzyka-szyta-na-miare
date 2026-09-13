76d697d477ddbf1a20b600b67ff3c2245dbf2047
Wojciech Przybyła <contact@wpww.pl>
2026-09-13 12:35:50 +0200

    💄 proper size of embedded video

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -91,7 +91,7 @@ function openSection(slug) {
             </ul>
         </div>
     </div>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/WYTOqc6ADwA?si=sYZUcVrL0Znh7czc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <iframe src="https://www.youtube.com/embed/WYTOqc6ADwA?si=sYZUcVrL0Znh7czc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 </section>
 
 <x-front.song-list.popup />
